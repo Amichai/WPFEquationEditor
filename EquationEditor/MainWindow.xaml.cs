d52dfdc325dedc8ade89cac27d5933a816251fcb
Amichai <a>
2013-08-27 21:21:08 -0400

    UI.

--- EquationEditor/MainWindow.xaml.cs
+++ EquationEditor/MainWindow.xaml.cs
@@ -22,7 +22,8 @@ namespace EquationEditor {
             InitializeComponent();
             //this.input.Text = @"y=3+4*2/(1-x)^2^3";
             //this.input.Text = @"add(3,4) / 5";
-            this.input.Text = @"3 + 4 * ( 3 / 22 - 22) ^ 3 ^4";
+            //this.input.Text = @"3 + 4 * ( 3 / 22 - 22) ^ 3 ^4";
+            this.input.Text = @"4 * 22 = (34 / (3 + 33 + 2^3^3))";
 
             this.input.BorderThickness = new Thickness(1, 1, 1, 1);
             this.input.BorderBrush = Brushes.Gray;
@@ -37,12 +38,15 @@ namespace EquationEditor {
         }
 
         private void update() {
+            if (this.input.Text == "") {
+                return;
+            }
             var queue = tokenizer.Tokenize(this.input.Text);
             tree.BuildTree(queue);
-            int secondToLast = this.resultStack.Children.Count - 1;
+            int insertIdx = this.resultStack.Children.IndexOf(this.input);
             var equation = tree.Root.GetElement();
             equation.Tag = this.input.Text;
-            this.resultStack.Children.Insert(secondToLast, equation);
+            this.resultStack.Children.Insert(insertIdx, equation);
             this.input.BorderBrush = Brushes.Gray;
             this.input.Text = "";
             this.lineNumber++;
@@ -54,17 +58,30 @@ namespace EquationEditor {
 
         int lineNumber = 0;
 
+        private void Swap(UIElementCollection col, int idx1, int idx2) {
+            var toTake1 = col[idx1];
+            var toTake2 = col[idx2];
+
+            col.Remove(toTake1);
+            col.Remove(toTake2);
+            col.Insert(idx2, toTake1);
+
+        }
+
         private void Window_KeyDown_1(object sender, KeyEventArgs e) {
             switch (e.Key) {
                 case Key.Enter:
-                    update();
-                    break;
-                case Key.Up:
-                    var text = (this.resultStack.Children[lineNumber] as StackPanel).Tag as string;
-                    this.resultStack.Children[lineNumber] = new TextBox() { Text = text };
+                    if (this.input.Text.Count() > 0) {
+                        update();
+                    } else {
+                        var text = (this.resultStack.Children[lineNumber - 1] as FrameworkElement).Tag as string;
+                        this.input.Text = text;
+                        this.input.CaretIndex = this.input.Text.Length;
+                        Swap(this.resultStack.Children, lineNumber, lineNumber - 1);
+                        lineNumber--;
+                    }
                     break;
             }
-
         }
     }
 }
